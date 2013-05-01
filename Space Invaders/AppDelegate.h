aa1bc679bd40cb2a9626af9a047089079229e024
ruben flores <rubenator.flores@gmail.com>
2013-05-01 16:03:33 -0500

    Initial Commit

---
+++ Space Invaders/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Space Invaders
+//
+//  Created by Ruben Flores on 5/1/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Space Invaders/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Space Invaders
+//
+//  Created by Ruben Flores on 5/1/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
