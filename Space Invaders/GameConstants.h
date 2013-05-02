0668cf18d74145afc53ad25e3d8e132803a1adc8
ruben flores <rubenator.flores@gmail.com>
2013-05-02 13:29:37 -0500

    Wrote skeletons for most of the game object classes

---
+++ Space Invaders/GameConstants.h
@@ -0,0 +1,14 @@
+//
+//  GameConstants.h
+//  Space Invaders
+//
+//  Created by Ruben Flores on 5/2/13.
+//
+//
+
+#ifndef Space_Invaders_GameConstants_h
+#define Space_Invaders_GameConstants_h
+
+
+
+#endif
