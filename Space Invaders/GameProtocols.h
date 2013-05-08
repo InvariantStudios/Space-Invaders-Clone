d9673e0f76f1ac2d15c95be3f205fba3e12ede5b
ruben flores <rubenator.flores@gmail.com>
2013-05-08 15:48:08 -0500

    Big Update

---
+++ Space Invaders/GameProtocols.h
@@ -0,0 +1,14 @@
+//
+//  GameProtocols.h
+//  Space Invaders
+//
+//  Created by Ruben Flores on 5/6/13.
+//
+//
+
+#ifndef Space_Invaders_GameProtocols_h
+#define Space_Invaders_GameProtocols_h
+
+
+
+#endif
---
+++ Space Invaders/InvaderFlock.m
@@ -0,0 +1,22 @@
+//
+//  AlienFlock.m
+//  Space Invaders
+//
+//  Created by Ruben Flores on 5/1/13.
+//
+//
+
+#import "AlienFlock.h"
+
+@implementation AlienFlock
+
+
+
+
+
+
+-(void) dealloc{
+    [super dealloc];
+}
+
+@end
